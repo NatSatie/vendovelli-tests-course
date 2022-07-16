88a246cef96ca25542f935b5a805a2de19cf310a
Natália Satie <naty.odashima@gmail.com>
2022-07-16 13:36:38 -0300

    Modulo 1: aula 7

--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -1,11 +1,31 @@
 const { queryString } = require('./queryString');
 
 describe('object to query string', () => {
-  it('it should create a val;id query string', () => {
+  it('it should create a valid query string', () => {
     let obj = {
       name: 'Fabio',
       carrer: 'developer',
     };
     expect(queryString(obj)).toBe('name=Fabio&carrer=developer');
+  });
+
+  it('it should create a valid string even with array', () => {
+    let obj = {
+      name: 'Fabio',
+      abilities: ['js', 'tdd'],
+    };
+    console.log(queryString(obj));
+    expect(queryString(obj)).toBe('name=Fabio&abilities=js,tdd');
+  });
+
+  it('it should return an error when objec is passed as value ', () => {
+    let obj = {
+      name: 'Fabio',
+      abilities: {
+        first: 'js',
+        second: 'tdd',
+      },
+    };
+    expect(() => queryString(obj)).toThrowError();
   });
 });
